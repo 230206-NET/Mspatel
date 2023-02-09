98a838a1f50dd3a2cdfdfbdf5ac68f09e61a60d7
Meet Patel <meetpatel3299@icloud.com>
2023-02-09 17:06:44 -0600

    Hangman and budgetapp from yester push changes.

---
+++ BudgetApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace BudgetApp
+{
+    public class BA
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("BudgetApp Start");
+
+            Console.WriteLine("Enter Your Starting Balance");
+            int balance = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Starting Balance is: $" + balance);
+            while(true)
+            {
+                Console.WriteLine("Amount of Expense: ");
+                int expense = 0;
+                int tempExpense = int.Parse(Console.ReadLine());
+                if(tempExpense > balance)
+                {
+                    Console.WriteLine("Expense s greater then Account Balance and Can't be Accepted");
+                }
+                else
+                {
+                    expense = tempExpense;
+                    balance = balance - expense;
+                    Console.WriteLine(balance);
+                }
+                Console.WriteLine("Are There More Expenses? [y/n]");
+                string response = Console.ReadLine();
+                if(response == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("Account Balance: $" + balance);
+
+        }
+    }
+}
+
