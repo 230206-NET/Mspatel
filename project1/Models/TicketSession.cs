68759487b4b228497917b8e6e575c6afc4b3bdc1
Meet Patel <meetpatel3299@icloud.com>
2023-02-16 14:38:56 -0600

    Made skeleton for project 1 as a start

---
+++ project1/Models/TicketSession.cs
@@ -0,0 +1,43 @@
+using Models.CustomException;
+using System.Text;
+
+namespace Models;
+    public class TicketSession
+    {
+        public TicketSession()
+        {
+            TicketForm = new List<Ticket>();
+        }
+
+        public DateTime TicketDate { get; set; } = DateTime.Now;
+        private string _ticketName = DateTime.Now.ToString();
+        public string TicketName 
+        {
+            get{
+                return _ticketName;
+            }
+            set{
+                if(value.Length >= 100){
+                    throw new ArgumentLengthException("Name must be less than 100 characters");
+                }
+                if(!string.IsNullOrWhiteSpace(value)){
+                    _ticketName = value;
+                }
+            }
+        }
+        public List<Ticket> TicketForm { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            
+            sb.Append($"Name: {this.TicketName}\nDate: {this.TicketDate}");
+
+            foreach(Ticket e in TicketForm) {
+                sb.Append("\n");
+                sb.Append(e.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
--- project1/UI/MainMenu.cs
+++ project1/UI/MainMenu.cs
@@ -1,23 +1,115 @@
-﻿using Login;
+﻿using DataAccess;
 namespace UI;
     public class MainMenu
     {
+        private List<TicketSession> tic = new();
         public void Start()
         {
-            LoginInfo log = new LoginInfo();
+            while(true) 
+            {
+                Console.WriteLine("Expense Tracker:");
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("[1] Create a new Ticket");
+                Console.WriteLine("[2] Modify an existing Ticket");
+                Console.WriteLine("[3] View all Tickets");
+                Console.WriteLine("[x] Exit");
 
-            Console.WriteLine("Username: ");
-            string user = Console.ReadLine();
-            Console.WriteLine("Password: ");
-            string pass = Console.ReadLine();
+                string input = Console.ReadLine();
+                switch(input)
+                {
+                    case "1":
+                        // TODO: add new ticket
+                        CreateNewTicket();
+                    break;
+                    
+                    case "2":
+                        // ToDo: Modify Existing Workout
+                    break;
+                    case "3":
+                        // TODO: Veiw all tickets
+                        List<TicketSession> sessions = new FileStorage().GetAllTicket();
+                        foreach(TicketSession s in sessions) {
+                            Console.WriteLine(s);
+                        }
+                    break;
 
-            log.Username = user;
-            log.Password = pass;
+                    case "x":
+                        Environment.Exit(0);
+                    break;
 
-            Console.WriteLine("User : " + user);
-            Console.WriteLine("Username : " + log.Username);
+                    default:
+                        Console.WriteLine("I don't understand your input");
+                    break;
+                }
+            }
+        }
+        private void CreateNewTicket()
+        {
+            TicketSession session = new TicketSession();
+            Console.WriteLine("Creating new Ticket: ");
+            Console.WriteLine("Ticket Date: ");
+            string? TicketDate = Console.ReadLine();
 
-            Console.WriteLine("Pass : " + pass);
-            Console.WriteLine("Password : " + log.Password);
+            DateTime parsed;
+            bool parseSuccess = DateTime.TryParse(TicketDate, out parsed);
+
+            if(parseSuccess)
+            {
+                 session.TicketDate = parsed;
+            }
+
+            while(true) 
+            {
+                Console.WriteLine("Ticket Name: ");
+
+                string nameInput = Console.ReadLine();
+                try {
+                    session.TicketName = nameInput;
+                    break;
+                }
+                catch(ArgumentLengthException ex) {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+            }
+            while(true) 
+            {
+                Console.WriteLine("What did you do?");
+
+                string ticketName = Console.ReadLine();
+
+                Console.WriteLine("Any Description?");
+                string ticketNotes = Console.ReadLine();
+
+                Ticket ex = new Ticket {
+                    TicketName = ticketName,
+                    TicketDes = ticketNotes
+                };
+
+                session.TicketForm.Add(ex);
+                Console.WriteLine("Add more? [y/n]");
+                string more = Console.ReadLine();
+                if(more.ToLower()[0] == 'n') break;
+            }
+
+            new FileStorage().CreateNewSession(session);
+            Console.WriteLine(session);
         }
     }
+
+
+// LoginInfo log = new LoginInfo();
+
+            // Console.WriteLine("Username: ");
+            // string user = Console.ReadLine();
+            // Console.WriteLine("Password: ");
+            // string pass = Console.ReadLine();
+
+            // log.Username = user;
+            // log.Password = pass;
+
+            // Console.WriteLine("User : " + user);
+            // Console.WriteLine("Username : " + log.Username);
+
+            // Console.WriteLine("Pass : " + pass);
+            // Console.WriteLine("Password : " + log.Password);
