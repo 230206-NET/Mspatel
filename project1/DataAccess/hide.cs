88f721d6d55b429a6016cb6c0ab4372187a93263
Meet Patel <meetpatel3299@icloud.com>
2023-03-02 19:51:53 -0600

    Project api working

--- project1/DataAccess/hide.cs
+++
@@ -1,7 +0,0 @@
-namespace DataAccess;
-    internal class hide
-    {
-        private const string _connectionsqldb = "Server=tcp:workoutdb-server.database.windows.net,1433;Initial Catalog=workoutDBSQL;Persist Security Info=False;User ID=workoutdb;Password=workout@12;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
-
-        public static string getdbConnection() => _connectionsqldb;
-    }
